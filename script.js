e326d55d8e48d8be71bfb6349f700ce5abdce8d0
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-03-05 13:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student={
+	name:"balram",
+	age:24,
+	city:"aplwal"
+}
+
+function getKeys(obj) {
+	return Object.keys(obj)
+}
+
+console.log(getKeys(student))
